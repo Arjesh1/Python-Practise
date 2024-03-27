bcae60e83fcb6b80103f0bfa869bae09d124f2de
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-03-28 09:44:08 +1100

    function in python

--- main.py
+++ main.py
@@ -19,19 +19,28 @@ userData = {
 }
 
 #  Prints a string into the console, and asks the user for a string input.
-info = input('What do you want to know?')
+currentUser = input('What is your name?')
+print(f'Hello {currentUser}')
+info = input('What do you want to know about Arjesh?')
 
 # CONDITIONALS
 if info == 'name':
     print('His name is '+ name)
 elif info == 'experience':
-    print('His experience is '+ userData['experience'])
+    print('His experience is '+ str(userData['experience']) + ' yrs')
 elif info == 'isDeveloper' and userData['is_Developer'] == True:
     print('Yes, he is a developer')
 elif info == 'skills':
-    print('His name is '+ ','.join(userData['skills']))
+    print('His skill are '+ ','.join(userData['skills']))
 else:
     print('No record found')
+
+# function with parameters
+def add_function(a, b):
+    return a + b
+
+print(add_function(1,2))
+
 
 
 
