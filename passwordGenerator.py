9fa775317692a5bcf27836682a847fda66d6c3a1
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-03-29 09:55:54 +1100

    uppercase choice added to password generator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,17 +1,22 @@
 import random
 
-letter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '|', ':', ';', ',', '.', '/', '?', '~']
 
 print('Welcome to Python password generator.')
-passwordLength = int(input('Length of password '))
-totalSymbol = int(input('Numbers of symbol '))
-totalNumber = int(input('Numbers of number '))
+passwordLength = int(input('Length of password - '))
+totalSymbol = int(input('How many symbols do you want? - '))
+totalNumber = int(input('How many numbers do you want? - '))
+totalUppercase = int(input('How many uppercase do you want? - '))
 
 password_list = []
-for character in range(1, passwordLength - totalNumber - totalSymbol + 1):
+for character in range(1, passwordLength - totalNumber - totalSymbol- totalUppercase + 1):
     password_list += random.choice(letter)
+
+for character in range(1, totalUppercase + 1):
+    password_list += random.choice(upperCase)
 
 for character in range(1, totalSymbol + 1):
     password_list += random.choice(symbols)
